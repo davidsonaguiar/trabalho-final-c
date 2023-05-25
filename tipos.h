11c25b3c579b281485633cb6034f3a63b7a73f50
davidson.05@outlook.com <davidson.05@outlook.com>
2023-05-25 12:36:59 -0300

    tipos

--- tipos.h
+++ tipos.h
@@ -11,16 +11,25 @@ typedef struct {
 
 typedef struct {
     char matricula[10];
-    char CPF[13];
-    char nome[100];
+    char cpf[12];
+    char nome[50];
     Endereco endereco;
 } Aluno;
 
 typedef struct {
     char matricula[10];
-    char cpf[13];
-    char nome[100];
+    char cpf[12];
+    char nome[50];
     Endereco endereco;
 } Professor;
+
+typedef struct {
+    char codigo[10];
+    char nome[50];
+    Professor professor;
+    Aluno alunos[40];
+    int numAlunos;
+    float media;
+} Turma;
 
 #endif
