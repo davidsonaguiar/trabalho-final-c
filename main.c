fb9e482ee3573659eb607e07bbf531806593d111
davidson.05@outlook.com <davidson.05@outlook.com>
2023-05-25 11:32:34 -0300

    atualizar

--- main.c
+++ main.c
@@ -1,40 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "./menus/menus.h"
 
-int main(int argc, char const *argv[])
-{
 
-    int opcao;
-
-    do {
-        printf("==== Menu principal ====\n\n");
-        printf("1 - Menu do aluno\n");
-        printf("2 - Menu do professor\n");
-        printf("0 - Sair\n");
-
-        printf("Escolha uma das opcoes acima: ");
-        scanf("%d", &opcao);
-        getchar();
-
-        switch (opcao)
-        {
-        case 1:
-            menu_aluno();
-            break;
-
-        case 2:
-            menu_professor();
-            break;
-            
-        case 0:
-            printf("Finalizando app...\n\n");
-            return 0;
-            break;
-        
-        default:
-            printf("Opcao inválida!! Tente novamente\n\n");
-            break;
-        }
-        
-    } while(1);
+int main() {
+    menuPrincipal();
 }
