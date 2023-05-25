6a2081efafcace3f6444c8bf726669bf84a7c05a
davidson.05@outlook.com <davidson.05@outlook.com>
2023-05-25 12:40:32 -0300

    menus

--- menus/menus.h
+++ menus/menus.h
@@ -2,11 +2,8 @@
 #define _MENUS_H
 
 #include "../dados/professor/professor.h"
-<<<<<<< HEAD
 #include "../dados/aluno/aluno.h"
-=======
 #include "../dados/turma/turma.h"
->>>>>>> cb03f81b492e77e64c3f29278423002049d6556a
 
 void menuPrincipal();
 void menuAluno();
