ece14135f40db5e5f9a31d800faf8a6f5bdd6885
Caymi <[caymiferreira@gmail.com]>
2023-05-28 19:28:48 -0300

    adição de comentários em menu_aluno.c

--- menus/menu_aluno.c
+++ menus/menu_aluno.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-#include "./menus.h"
+#include <stdio.h>       // Inclui a biblioteca de entrada/saída padrão
+#include "./menus.h"     // Inclui o arquivo de cabeçalho 'menus.h'
 
 void menuAluno() {
-
     int opcao;
 
     do {
@@ -10,40 +9,40 @@ void menuAluno() {
         printf("| ===== MENU ALUNO ===== |\n");
         printf("+------------------------+\n");
 
-        printf("1 - Cadastrar Aluno\n");
-        printf("2 - Mostrar Alunos\n");
-        printf("3 - Atualizar Aluno\n");
-        printf("4 - Remover Aluno\n");
-        printf("0 - Menu Principal\n\n");
+        printf("1 - Cadastrar Aluno\n");    // Opção para cadastrar um aluno
+        printf("2 - Mostrar Alunos\n");     // Opção para mostrar os alunos cadastrados
+        printf("3 - Atualizar Aluno\n");    // Opção para atualizar os dados de um aluno
+        printf("4 - Remover Aluno\n");      // Opção para remover um aluno
+        printf("0 - Menu Principal\n\n");  // Opção para retornar ao menu principal
 
-        printf("Escolha uma das opcoes acima: ");
-        scanf("%d", &opcao);
+        printf("Escolha uma das opcoes acima: ");   // Solicita ao usuário que escolha uma opção
+        scanf("%d", &opcao);                        // Lê a opção escolhida pelo usuário
 
         printf("\n");
 
         switch (opcao) {
             case 1:
-                cadastrarAluno();
+                cadastrarAluno();    // Chama a função para cadastrar um aluno
                 break;
             
             case 2:
-                imprimirAlunos();
+                imprimirAlunos();    // Chama a função para imprimir os alunos cadastrados
                 break;
             
             case 3:
-                atualizarAluno();
+                atualizarAluno();    // Chama a função para atualizar os dados de um aluno
                 break;
             
             case 4:
-                descadastrarAluno();
+                descadastrarAluno(); // Chama a função para remover um aluno
                 break;
             
             case 0:
-                return;
+                return;              // Retorna ao menu principal
                 break;
             
             default:
                 break;
-            }
-    } while(1);
+        }
+    } while(1);  // Loop infinito para manter o menu em execução
 }
