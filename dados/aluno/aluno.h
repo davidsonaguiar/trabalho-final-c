0f99d1631c32bc57879c559e658c9a148157c4ae
Caymi <[caymiferreira@gmail.com]>
2023-05-28 19:21:52 -0300

    adição de comentários em aluno.h

--- dados/aluno/aluno.h
+++ dados/aluno/aluno.h
@@ -1,12 +1,12 @@
 #ifndef _ALUNOS_H
 #define _ALUNOS_H
 
-#include "../../constantes.h"
-#include "../../tipos.h"
+#include "../../constantes.h"   //inclui o arquivo de constantes
+#include "../../tipos.h"        //inclui o arquivo de definições de tipos
 
-void cadastrarAluno();
-void imprimirAlunos();
-void atualizarAluno();
-void descadastrarAluno();
+void cadastrarAluno();          //declaração da função cadastrarAluno
+void imprimirAlunos();          //declaração da função imprimirAlunos
+void atualizarAluno();          //declaração da função atualizarAluno
+void descadastrarAluno();       //declaração da função descadastrarAluno
 
 #endif
