b96f8a6114fe3d10e8087310255cc3761cfea74c
hebertivisson <hias1@aluno.ifal.edu.br>
2023-05-28 22:34:43 -0300

    comentarios no menu principal

--- menus/menu_principal.c
+++ menus/menu_principal.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include "./menus.h"
-
+#include <stdio.h> // Inclui a biblioteca de entrada/saída padrão
+#include "./menus.h" // Inclui o arquivo de cabeçalho 'menus.h'
+// Sistema de para o menu principal.
 void menuPrincipal() {
 
     int opcao;
@@ -9,12 +9,12 @@ void menuPrincipal() {
         printf("\n+----------------------------+\n");
         printf("| ===== MENU PRINCIPAL ===== |\n");
         printf("+----------------------------+\n");
-
+// Menssagens do menu principal.
         printf("1 - Menu do Aluno\n");
         printf("2 - Menu do Professor\n");
         printf("3 - Menu da Turma\n");
         printf("0 - Sair\n");
-
+// Solicitando escolha.
         printf("Escolha uma das opcoes acima: ");
         scanf("%d", &opcao);
 
@@ -22,25 +22,25 @@ void menuPrincipal() {
 
         switch (opcao) {
             case 1:
-                menuAluno();
+                menuAluno(); // Chamando menu aluno.
                 break;
             
             case 2:
-                menuProfessor();
+                menuProfessor(); // Chamando menu professor.
                 break;
             
             case 3:
-                menuTurma();
+                menuTurma(); // Chamando menu turma.
                 break;
             
             case 0:
-                printf("Saindo...\n");
+                printf("Saindo...\n"); // função para sair e apresentar menssagem.
                 return;
                 break;
             
             default:
-                printf("Opcao Invalida!\n");
+                printf("Opcao Invalida!\n"); // Casso nenhuma das opções seja escolhida.
                 break;
         }
-    } while(1);
+    } while(1); //loop para manter o menu em execução.
 }
